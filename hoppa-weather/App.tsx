653635ded4ba0e210de003cd51691f08c3592b1d
Thomas Henderson <tbhenderson55@yahoo.com>
2023-03-07 12:43:51 +0100

    navigation stack

--- hoppa-weather/App.tsx
+++ hoppa-weather/App.tsx
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import WeatherDetail from './screens/WeatherDetail';
+import WeatherList from './screens/WeatherList';
+import { WeatherStackParams } from './WeatherStackParams';
+
+const Stack = createNativeStackNavigator<WeatherStackParams>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='WeatherList'>
+        <Stack.Screen name='WeatherList' component={WeatherList} />
+        <Stack.Screen
+          name='WeatherDetail'
+          // options={{
+          //   header: () => null,
+          // }}
+          component={WeatherDetail}
+        />
+      </Stack.Navigator>
+
+      {/* <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style='auto' />
+      </View> */}
+    </NavigationContainer>
   );
 }
 
---
+++ hoppa-weather/WeatherStackParams.ts
@@ -0,0 +1,4 @@
+export type WeatherStackParams = {
+  WeatherDetail: any;
+  WeatherList: undefined;
+};
